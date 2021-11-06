581ff5bb9e49e791897b8b82db8f7d713afb6f0c
Karim Rhoualem <karim.rhoualem@gmail.com>
2021-11-06 17:14:54 -0400

    Added logic for getting cuisines and reviews for a specific restaurant.

--- restaurant_reviews/backend/api/restaurants.controller.js
+++ restaurant_reviews/backend/api/restaurants.controller.js
@@ -1,7 +1,7 @@
 import RestaurantsDAO from "../dao/restaurantsDAO.js"
 
 export default class RestaurantsController {
-	
+
 	static async apiGetRestaurants(req, res, next) {
 		const restaurantsPerPage = req.query.restaurantsPerPage ? parseInt(req.query.restaurantsPerPage, 10) : 20
 		const page = req.query.page ? parseInt(req.query.page, 10) : 0
@@ -32,5 +32,34 @@ export default class RestaurantsController {
 		}
 
 		res.json(response)
+	}
+
+	static async apiGetRestaurantById(req, res, next) {
+		try {
+			let id = req.params.id || {}
+			let restaurant = await RestaurantsDAO.getRestaurantByID(id)
+			
+			if (!restaurant) {
+				res.status(404).json({ error: "Not found" })
+				return
+			}
+			
+			res.json(restaurant)
+		} 
+		catch (e) {
+			console.log(`api, ${e}`)
+			res.status(500).json({ error: e })
+		}
+	}
+
+	static async apiGetRestaurantCuisines(req, res, next) {
+		try {
+			let cuisines = await RestaurantsDAO.getCuisines()
+			res.json(cuisines)
+		} 
+		catch (e) {
+			console.log(`api, ${e}`)
+			res.status(500).json({ error: e })
+		}
 	}
 }
